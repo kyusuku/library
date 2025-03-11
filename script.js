70caea48b094b265259ed0c7184f060e3a922bd2
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-03-11 20:13:37 +0700

    Add remove book and toggle read functionality

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const newBookBtn = document.querySelector('.newBookBtn');
 const dialog = document.querySelector('dialog');
 const dialogCloseBtn = document.querySelector('dialog button');
 const dialogConfirm = document.querySelector('.confirmBtn');
+// const removeBook = document.getElementById('removeBook');
 
 let userTitle;
 let userAuthor;
@@ -25,26 +26,39 @@ function addBookToLibrary(title, author, numberOfPages, hasRead) {
 
 function displayBooks() {
     for (const book of myLibrary) {
+        let removeBookBtn = document.createElement('button');
+
         let bookCard = document.createElement('div');
+        bookCard.setAttribute('data-id', book.id);
+
         let bookId = document.createElement('div');
         let bookTitle = document.createElement('div');
         let bookAuthor = document.createElement('div');
         let bookPages = document.createElement('div');
-        let bookRead = document.createElement('div');
+        let bookRead = document.createElement('button');
 
+        removeBookBtn.setAttribute('id', 'removeBook');
+        bookRead.setAttribute('id', 'readBtn');
         bookId.textContent = book.id;
         bookTitle.textContent = book.title;
         bookAuthor.textContent = book.author;
         bookPages.textContent = book.pages + ' Pages';
-        bookRead.textContent = book.read;
 
-    
+        if (book.read) {
+            bookRead.textContent = "Read";
+            bookRead.style.backgroundColor = "#608860";
+        } else {
+            bookRead.textContent = "Not Read"
+            bookRead.style.backgroundColor = "#ffc2c2";
+        }
+
+        bookCard.appendChild(removeBookBtn);
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
         bookCard.appendChild(bookPages);
         bookCard.appendChild(bookRead);
-        bookCard.appendChild(bookId);
 
+        card.setAttribute('data-attribute', book.id);
         card.appendChild(bookCard);
     }
 }
@@ -79,6 +93,25 @@ dialogConfirm.addEventListener('click', (event) => {
     addBookToLibrary(userTitle, userAuthor, userPages, userRead);
     card.textContent = "";
     displayBooks();
+});
 
+card.addEventListener('click', (e) => {
+    if (e.target.id === 'removeBook') {
+        let bookCard = e.target.parentElement;
+        let bookId = bookCard.getAttribute('data-id');
 
+        myLibrary.splice(myLibrary.findIndex(book => book.id === bookId), 1);
+
+        bookCard.remove();
+    }
+
+    if (e.target.id === 'readBtn') {
+        if (e.target.textContent === "Read") {
+            e.target.textContent = "Not Read";
+            e.target.style.backgroundColor = "#ffc2c2";
+        } else {
+            e.target.textContent = "Read";
+            e.target.style.backgroundColor = "#608860";
+        }
+    }
 });
